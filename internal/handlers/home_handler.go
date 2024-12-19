8cd55cc84e05cce8305341dc0d66f06e145e9b53
myshkins <alex@ak0.io>
2024-12-18 19:26:58 -0800

    add old webpage stuff

--- internal/handlers/home_handler.go
+++ internal/handlers/home_handler.go
@@ -6,21 +6,14 @@ import (
 	"net/http"
 )
 
-// func HandleHome(logger *log.Logger) {
-
-//   return http.HandlerFunc{
-//     func(w http.ResponseWriter, r *http.Request)
-//   }
-// }
-
 func HandleHome(logger *log.Logger) http.Handler {
-//   // do necessary prep work here
+  // do necessary prep work here
   log.Println("HandleHome was called")
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-      // handle the request here
       fmt.Fprintf(w, "hewo myshy! you requested: %s\n", r.URL.Path)
       logger.Println("handling home")
 		},
 	)
 }
+
