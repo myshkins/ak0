8af2e833da692a94d0170ac1b0d70c80463911d1
myshkins <alex@ak0.io>
2025-01-17 12:40:10 -0800

    flake devshell setup

--- flake.nix
+++ flake.nix
@@ -1,19 +1,46 @@
 {
   description = "ak0_2 go backend flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
+  # inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default =
+  outputs = { self, nixpkgs }: 
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in pkgs.buildGoModule {
-      pname = "ak0_2";
-      version = "0.0.1";
-      src = ./.;
-      venderSha256 = null;
+
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+      # pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+    in {
+      
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          go-backend = pkgs.buildGoModule {
+            pname = "ak0_2";
+            inherit version;
+            src = ./.;
+            vendorHash = null;
+          };  
+        }
+      );
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go gopls gotools go-tools ];
+          };
+        });
     };
-  };
 }
+
