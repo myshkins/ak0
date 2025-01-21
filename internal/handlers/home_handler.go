ff1b518593ac63ed97ead8d0d44a7b55608a8c9c
myshkins <alex@ak0.io>
2025-01-21 12:24:02 -0800

    update fs path

--- internal/handlers/home_handler.go
+++ internal/handlers/home_handler.go
@@ -8,7 +8,8 @@ import (
 func HandleHome(logger *slog.Logger) http.Handler {
   // do necessary prep work here
   logger.Info("HandleHome was called")
-  fs := http.FileServer(http.Dir("/home/myshkins/projects/ak0_2/web/dist"))
+  fs := http.FileServer(http.Dir("/web/dist"))
+  // fs := http.FileServer(http.Dir("/home/myshkins/projects/ak0_2/web/dist"))
   fsWrapper := func() http.Handler {
     logger.Info("fsWrapper was called")
     return fs
