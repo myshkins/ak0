617b041cb14a6c861f1fc0866ccd0afd8b5ae3b1
myshkins <alex@ak0.io>
2025-02-13 16:07:30 -0800

    middle of certs and stuff

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
+script=$(readlink -f "$0")
+script_path=$(dirname "$script")
+cd $script_path
+
+# create this hacky run location that avoids needing sudo
+mkdir -p /home/iceking/.local/ak0/
+
+# copy everything to run location
+cp -r \
+  compose.yaml \
+  compose.prod.yaml \
+  configs \
+  grafana \
+  /home/iceking/.local/ak0/
 
