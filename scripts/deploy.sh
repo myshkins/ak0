7f9f43239739175e6f3c39a320b7b5a1bf49308b
myshkins <alex@ak0.io>
2025-02-24 14:44:10 -0800

    fix grafana data source provisioning

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,12 +11,15 @@ set -e
 
 # run the build image script that was created above
 cd .. >/dev/null
+echo "running ./result to build image"
 ./result > ./build/backend/ak0_image
 
-scp ./build/backend/ak0_image pgum:/home/iceking/data/ak0/images/
-scp ./.env pgum:/home/iceking/.local/ak0/
+echo "copying image and .env to pgum"
+scp -F /home/myshkins/.ssh/config ./build/backend/ak0_image pgum:/home/iceking/data/ak0/images/
+scp -F /home/myshkins/.ssh/config ./.env pgum:/home/iceking/.local/ak0/
 
-ssh pgum << 'EOF'
+echo "executing deploy steps on pgum"
+ssh -F /home/myshkins/.ssh/config pgum << 'EOF'
   cd /home/iceking/.local/ak0/
   docker compose --profile full -f compose.yaml -f compose.prod.yaml down
   docker image rm ak0:latest
@@ -27,12 +30,12 @@ ssh pgum << 'EOF'
   docker compose --profile full -f compose.yaml -f compose.prod.yaml up -d
 EOF
 
-ssh rpgum << 'EOF'
+ssh -F /home/myshkins/.ssh/config rpgum << 'EOF'
   /home/iceking/apps/ak0/configs/create_certs.sh
   /home/iceking/apps/ak0/scripts/export.sh
 EOF
 
-ssh pgum << 'EOF'
+ssh -F /home/myshkins/.ssh/config pgum << 'EOF'
   cd /home/iceking/.local/ak0/
   docker compose --profile full -f compose.yaml -f compose.prod.yaml up -d
 EOF
