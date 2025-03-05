3c0a1aaf9b7b057f1aea3686e472e928408d93db
myshkins <alex@ak0.io>
2025-03-04 20:54:25 -0800

    add grafana link to homepage

--- web/src/components/NavBar.jsx
+++ web/src/components/NavBar.jsx
@@ -9,6 +9,7 @@ const NavBar = () => {
                 <li><a href="https://blog.ak0.io" className="nav-link">blog</a></li>
                 <li><a href="https://blog.ak0.io" className="nav-link">contact</a></li>
                 <li><a href="/resume_Alex_Krenitsky.pdf" className="nav-link">resume</a></li>
+                <li><a href="/grafana/d/eeer2i27cwm4gb/ak0-overview%3ForgId%3D1%26from%3Dnow-24h%26to%3Dnow%26timezone%3Dbrowser" className="nav-link">site metrics</a></li>
             </ul>
         </div>
     )
